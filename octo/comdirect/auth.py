519de4acadee282ab1511974276cc387f1e16bc0
bciobo <bogdan@thanksben.com>
2023-08-02 19:26:55 +0200

    feat: refactor comdirect auth flow

--- octo/comdirect/auth.py
+++ octo/comdirect/auth.py
@@ -5,6 +5,7 @@ import uuid
 from dataclasses import dataclass
 from enum import Enum
 from pprint import pprint as pp
+from typing import Tuple, Optional, Any
 
 import requests
 from dotenv import dotenv_values
@@ -62,33 +63,34 @@ class ComdirectAuth:
         "Content-Type": "application/x-www-form-urlencoded",
     }
 
-    def __init__(self, account=ComdirectAccount.GEMEINSCHAFT):
+    def __init__(self, tan_wait_time=60, account=ComdirectAccount.GEMEINSCHAFT):
         self.account = account
         self._session = Session(str(uuid.uuid4()), str(uuid.uuid4()))
         self._config = None
-        self._token: TokenResponse = None
-        self._session_response: SessionStatusResponse = None
+        self._token: Optional[TokenResponse] = None
+        self._session_response: Optional[SessionStatusResponse] = None
         self._tan_challenge_id = None
+        self.tan_wait_time = tan_wait_time
 
     @property
-    def config(self):
+    def config(self) -> Config:
         if not self._config:
             self._config = self._parse_config(dotenv_values())
         return self._config
 
     @property
-    def token(self):
+    def token(self) -> Optional[TokenResponse]:
         return self._token
 
     @property
-    def session(self):
+    def session(self) -> Session:
         return self._session
 
     @property
-    def session_response(self):
+    def session_response(self) -> Optional[SessionStatusResponse]:
         return self._session_response
 
-    def _parse_config(self, config: dict):
+    def _parse_config(self, config: dict) -> Config:
         return Config(
             client_id=config.get("COMDIRECT_CLIENT_ID"),
             client_secret=config.get("COMDIRECT_CLIENT_SECRET"),
@@ -98,22 +100,31 @@ class ComdirectAuth:
             base_api_url=config.get("COMDIRECT_BASE_API_URL"),
         )
 
-    def oauth_flow(self):
+    def oauth_flow(self) -> None:
         self._token = self.auth_resource_owner_password_flow()
-        pp(self.token)
-        pp(self.session)
         self._session_response = self.fetch_session_status()
-        pp(self.session_response)
         if not self.session_response.sessionTanActive:
-            time.sleep(2)
             logger.info("Requesting TAN-Challenge")
             self._session_response, self._tan_challenge_id = self.request_tan_challenge()
-            pp(self.session_response)
-            pp(self._tan_challenge_id)
             logger.info("Sleeping for 60sec while the PhotoTAN app is used for TAN-Challenge")
-            time.sleep(60)
+            time.sleep(self.tan_wait_time)
             self._session_response = self.activate_tan()
             pp(self.session_response)
+
+    def _make_request(
+        self,
+        url: str,
+        headers: dict,
+        method: str = "GET",
+        data: Optional[str] = None,
+    ) -> Any:
+        try:
+            response = requests.request(method, url, headers=headers, data=data)
+            if not response.ok:
+                raise ComdirectAuthenticationFailed(response.text)
+            return response.json()
+        except Exception as e:
+            raise ComdirectAuthenticationFailed(e.args[0])
 
     def auth_resource_owner_password_flow(self) -> TokenResponse:
         oauth_url = f"{self.config.base_url}oauth/token"
@@ -145,16 +156,10 @@ class ComdirectAuth:
             "Authorization": self._make_token_header(),
             "x-http-request-info": json.dumps(request_info),
         }
-
-        response = requests.request("GET", session_url, headers=headers)
-
-        if not response.ok:
-            raise ComdirectAuthenticationFailed(response.text)
-
-        data = response.json()
+        data = self._make_request(session_url, headers)
         return SessionStatusResponse(**data[0])
 
-    def request_tan_challenge(self) -> SessionStatusResponse:
+    def request_tan_challenge(self) -> Tuple[SessionStatusResponse, str]:
         url = f"{self.config.base_api_url}session/clients/user/v1/sessions/{self.session_response.identifier}/validate"
         request_info = {"clientRequestId": {"sessionId": self.session.session_id, "requestId": self.session.request_id}}
         headers = {
@@ -164,16 +169,11 @@ class ComdirectAuth:
             "x-http-request-info": json.dumps(request_info),
         }
         payload = {"identifier": self.session_response.identifier, "sessionTanActive": True, "activated2FA": True}
-        pp(url)
-        pp(headers)
-        pp(payload)
         response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
-        pp(vars(response.request))
         if not response.ok:
             raise ComdirectAuthenticationFailed(response.text)
 
         header = json.loads(response.headers.get("x-once-authentication-info"))
-        pp(header)
         data = response.json()
         return SessionStatusResponse(**data), header["id"]
 
@@ -190,10 +190,5 @@ class ComdirectAuth:
             "x-once-authentication": "000000",
         }
         payload = {"identifier": self.session_response.identifier, "sessionTanActive": True, "activated2FA": True}
-
-        response = requests.request("PATCH", url, headers=headers, data=json.dumps(payload))
-        if not response.ok:
-            raise ComdirectAuthenticationFailed(response.text)
-
-        data = response.json()
+        data = self._make_request(url, headers, method="PATCH", data=json.dumps(payload))
         return SessionStatusResponse(**data)
